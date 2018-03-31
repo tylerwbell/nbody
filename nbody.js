47944e5dd2ac2086a11a8752a346820c20299ade
Tyler Bell <tyler@tylerwbell.com>
2018-03-31 15:47:50 -0400

    octree display

--- nbody.js
+++ nbody.js
@@ -1,6 +1,9 @@
 const combine = false;
 const drawVelocity = true;
 
+const PointType = "point";
+const TreeType = "tree";
+
 const points = [];
 
 function makePoint() {
@@ -13,6 +16,50 @@ function makePoint() {
     fY: 0,
     mass: Math.random() * 5,
   };
+}
+
+function makeTree(x, y, width, height, points) {
+  function subdivide(x, y, width, height) {
+    const p = [];
+    for (let i = 0; i < points.length; i += 1) {
+      const point = points[i];
+      if (point.x > x && point.x < (x + width)
+        && point.y > y && point.y < (y + height)) {
+        p.push(point);
+      }
+    }
+
+    return makeTree(x, y, width, height, p);
+  }
+
+  let nodes;
+  if (points.length <= 1) {
+    nodes = {
+      type: PointType,
+      points: points
+    }
+  } else {
+    const midX = width / 2;
+    const midY = height / 2;
+
+    nodes = {
+      type: TreeType, 
+      trees: [
+        subdivide(x, y, midX, midY),
+        subdivide(x + midX, y, midX, midY),
+        subdivide(x, y + midY, midX, midY),
+        subdivide(x + midX, y + midY, midX, midY)
+      ]
+    };
+  }
+
+    return {
+      x: x, 
+      y: y,
+      width: width,
+      height: height,
+      nodes: nodes
+    }
 }
 
 for (let i = 0; i < 900; i += 1) {
@@ -72,6 +119,57 @@ function update(points) {
     point.x += (t * point.vX);
     point.y += (t * point.vY);
   }
+
+}
+
+function renderPoint(ctx, point) {
+  const width = canvas.width;
+  const height = canvas.height;
+
+  const x = width * (point.x / 1000);
+  const y = height * (point.y / 1000);
+  const radius = Math.max(1, point.mass);
+
+  if (drawVelocity) {
+    const t = 100;
+    ctx.strokeStyle = "#ff00ff";
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + t * point.vX, y + t * point.vY);
+    ctx.stroke();
+  }
+
+  ctx.strokeStyle = "#ffff00";
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, 2 * Math.PI);
+  ctx.stroke();
+}
+
+function renderTree(ctx, tree) {
+  const width = canvas.width;
+  const height = canvas.height;
+
+  ctx.strokeStyle = "#00ffff";
+  ctx.globalAlpha = 0.1;
+  ctx.strokeRect(
+    width * tree.x / 1000, 
+    height* tree.y / 1000, 
+    width * tree.width / 1000, 
+    height * tree.height / 1000
+  );
+  ctx.globalAlpha = 1.0;
+
+  if (tree.nodes !== null) {
+    if (tree.nodes.type === PointType) {
+      for (point of tree.nodes.points) {
+        renderPoint(ctx, point);
+      }
+    } else if (tree.nodes.type === TreeType) {
+      for (tree of tree.nodes.trees) {
+        renderTree(ctx, tree);
+      }
+    }
+  }
 }
 
 function render(points) {
@@ -79,39 +177,15 @@ function render(points) {
   const height = canvas.height;
   const ctx = canvas.getContext("2d");
   ctx.fillStyle = "000000";
-  ctx.globalAlpha = 1.0;
+  ctx.globalAlpha = 0.5;
   ctx.fillRect(0, 0, width, height);
   ctx.globalAlpha = 1.0;
 
-  for (let i = 0; i < points.length; i += 1) {
-    const point = points[i];
-
-    const x = width * (point.x / 1000);
-    const y = height * (point.y / 1000);
-    const radius = Math.max(1, point.mass);
-
-    if (drawVelocity) {
-      const t = 100;
-      ctx.strokeStyle = "#ffff00";
-      ctx.beginPath();
-      ctx.moveTo(x, y);
-      ctx.lineTo(x + t * point.vX, y + t * point.vY);
-      ctx.stroke();
-    }
-
-    ctx.strokeStyle = "#ffffff";
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2 * Math.PI);
-    ctx.stroke();
-  }
+  const tree = makeTree(0, 0, 1000, 1000, points);
+  renderTree(ctx, tree);
 }
 
 const canvas = document.getElementById("canvas");
-//document.body.onmousemove = (event) => {
-//  points[0].x = event.clientX;
-//  points[0].y = event.clientY;
-//  points[0].mass = 1000;
-//};
 
 function resize() {
   canvas.width = 2 * document.documentElement.clientWidth;
